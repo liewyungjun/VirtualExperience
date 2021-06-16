c0c53c96ab4e82fd10117eae06a45d7df1c14848
liewyungjun <liewyungjun@gmail.com>
2021-06-17 07:35:07 +0800

    added Singapore Art Museum

--- data.js
+++ data.js
@@ -1,40 +1,43 @@
 const Data = [
   {
-    "id": "0",
-    "title": "Singapore Philatelic Museum",
-    "description": "Singapore Philatelic Museum is the custodian and curator of Singapore's treasure of philatelic materials. The museum collections range from stamps and archival philatelic material of Singapore from the 1830s to present day, and stamps from member countries of the Universal Postal Union.",
-    "image": require("./assets/philatelicmuseum.jpg"),
-    "price": 0,
-    "link": "https://www.nhb.gov.sg/spm/who-we-are/resources/virtual-tours",
-    "coordinate": {
-	  	"latitude": 1.2936221316260519,
-		  "longitude": 103.84879194602308
-	  }
+    id: "0",
+    title: "Singapore Philatelic Museum",
+    description:
+      "Singapore Philatelic Museum is the custodian and curator of Singapore's treasure of philatelic materials. The museum collections range from stamps and archival philatelic material of Singapore from the 1830s to present day, and stamps from member countries of the Universal Postal Union.",
+    image: require("./assets/philatelicmuseum.jpg"),
+    price: 0,
+    link: "https://www.nhb.gov.sg/spm/who-we-are/resources/virtual-tours",
+    coordinate: {
+      latitude: 1.2936221316260519,
+      longitude: 103.84879194602308,
+    },
   },
   {
-    "id": "1",
-    "title": "Planet or Plastic?",
-    "description": "Want to learn more about the global plastic pollution crisis and its impact on our environment? Join our Museum Ambassador, Diona, on a virtual tour through our new Planet or Plastic? exhibition. Look at how some of these impactful photographs and infographics will leave you feeling informed about this generation defining issue and ready to make the choice on what to prioritise - planet or plastic?",
-    "image": require("./assets/planetpic.webp"),
-    "price": 0,
-    "link": "https://www.marinabaysands.com/museum/events/virtual-tour-planet-or-plastic.html",
-    "coordinate": {
-		  "latitude": 1.2855560432517972,
-		  "longitude": 103.86096858465896
-	  }
+    id: "1",
+    title: "Planet or Plastic?",
+    description:
+      "Want to learn more about the global plastic pollution crisis and its impact on our environment? Join our Museum Ambassador, Diona, on a virtual tour through our new Planet or Plastic? exhibition. Look at how some of these impactful photographs and infographics will leave you feeling informed about this generation defining issue and ready to make the choice on what to prioritise - planet or plastic?",
+    image: require("./assets/planetpic.webp"),
+    price: 0,
+    link: "https://www.marinabaysands.com/museum/events/virtual-tour-planet-or-plastic.html",
+    coordinate: {
+      latitude: 1.2855560432517972,
+      longitude: 103.86096858465896,
+    },
   },
   {
-    "id": "2",
-    "title": "Third Item",
-    "description": "description",
-    "image": require("./assets/planetpic.webp"),
-    "price": 5,
-    "link": "https://www.marinabaysands.com/museum/events/virtual-tour-planet-or-plastic.html",
-    "coordinate": {
-		  "latitude": 1.3855560432517972,
-		  "longitude": 103.86096858465896
-	  }
-  }
-]
+    id: "2",
+    title: "Singapore Art Museum",
+    description:
+      "The SAM Virtual Curator Tour series comprises 4 episodes where SAM curators lead an immersive walkthrough of selected works at past exhibitions and give insights into contemporary artworks featured at these exhibitions",
+    image: require("./assets/singaporeArtMuseum.jpg"),
+    price: 5,
+    link: "https://www.singaporeartmuseum.sg/art-events/events/virtual-curator-tours",
+    coordinate: {
+      latitude: 1.2987021869626436,
+      longitude: 103.8509612768522,
+    },
+  },
+];
 
-export default Data
+export default Data;
